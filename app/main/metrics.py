7207f9d40e942eb08dded65e165d367d05b3a0d5
Khusro Jaleel <kerneljack@gmail.com>
2022-08-04 23:53:27 +0100

    adding metrics to web page

--- app/main/metrics.py
+++ app/main/metrics.py
@@ -3,17 +3,14 @@ from prometheus_client import Summary, Counter, Histogram
 
 class Metrics:
     # success / total metrics
-    http_success_counter = Counter("http_access_success", "Description of counter")
-    http_total_counter = Counter("http_access_total", "Description of counter")
+    http_success_counter = Counter("http_successful_access", "Successful HTTP counts")
+    http_total_counter = Counter("http_total_access", "Total HTTP counts")
 
     # error tracking metrics
     http_4xx_errors_counter = Counter("http_access_error_4xx", "Description of counter")
     http_5xx_errors_counter = Counter("http_access_error_5xx", "Description of counter")
 
     # Create a metric to track time spent and requests made.
-    http_request_time_summary = Summary(
-        "request_processing_seconds_s", "Time spent processing request (Summary)"
-    )
     http_request_time_histogram = Histogram(
-        "request_processing_seconds_h", "Time spent processing request (Histogram)"
+        "request_processing_seconds", "Time spent processing request (Histogram)"
     )
--- app/main/routes.py
+++ app/main/routes.py
@@ -5,29 +5,25 @@ import time
 
 @bp.route("/", methods=["GET", "POST"])
 @bp.route("/index", methods=["GET", "POST"])
-@metrics.http_request_time_summary.time()
 @metrics.http_request_time_histogram.time()
 def index():
-    template = render_template("index.html", title="Home")
-
     metrics.http_success_counter.inc()
     metrics.http_total_counter.inc()
-    return template
+
+    return render_template("index.html", title="Home", metrics=metrics)
 
 
 @bp.route("/do_task", methods=["GET", "POST"])
-@metrics.http_request_time_summary.time()
 @metrics.http_request_time_histogram.time()
 def do_task():
     process_request(5)
-    template = render_template("do_task.html", title="Do task")
 
     metrics.http_success_counter.inc()
     metrics.http_total_counter.inc()
-    return template
+
+    return render_template("do_task.html", title="Do task", metrics=metrics)
 
 
-@metrics.http_request_time_summary.time()
 @metrics.http_request_time_histogram.time()
 def process_request(t):
     """A dummy function that takes some time."""
